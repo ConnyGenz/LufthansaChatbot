d2e9d000c3df0c8c195e21d962330c8e1f29915b
ConnyGenz <connygenz@aol.com>
2024-09-23 22:10:41 +0200

    added xlsx data files to data folder

---
+++ xlsx_to_csv.py
@@ -0,0 +1,13 @@
+import os
+
+import pandas as pd
+
+statistical_data_files = ["aktie", "leistungsdaten", "umsatz_ergebnis"]
+input_directory = "data/"
+
+for file in statistical_data_files:
+    complete_input_file_path = input_directory + file + ".xlsx"
+    read_file = pd.read_excel(complete_input_file_path)
+    complete_output_file_path = input_directory + file + ".csv"
+    read_file.to_csv(complete_output_file_path, index=None, header=True)
+
