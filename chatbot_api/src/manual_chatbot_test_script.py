0ce57d69041e5e9018c81a5b9d981e2e671ffc69
ConnyGenz <connygenz@aol.com>
2025-01-02 16:26:16 +0100

    added question to manual_testbot_test_script.py

--- chatbot_api/src/manual_chatbot_test_script.py
+++ chatbot_api/src/manual_chatbot_test_script.py
@@ -3,4 +3,5 @@ dotenv.load_dotenv()
 from chatbot_api.src.agents.lufthansa_rag_agent import lufthansa_qa_agent_executor
 
 question = "Wie viele Flugzeuge welchen Flugzeugtyps möchte die Lufthansa bis 2027 anschaffen?"
+question= "Welche Positionen könnten Max Strobel und Nina Hagenau potentiell bei der Lufthansa innehaben?"
 response = lufthansa_qa_agent_executor.invoke({"input": question})
